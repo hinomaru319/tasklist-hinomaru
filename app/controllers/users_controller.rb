8a6909d4bf7d37a5cedc260d89972a45816bacad
hinomaru <ythinoma@gmai.com>
2021-03-11 11:53:02 +0000

    user and login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザーの登録に成功しました"
+      redirect_to "/"
+    else
+      flash.now[:danger] = "ユーザーの登録に失敗しました"
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
